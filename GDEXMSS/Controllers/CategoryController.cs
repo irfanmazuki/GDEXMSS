60aa4ac7ba45e805abf2f1cfe4cbee22875c62a2
irfanmazuki <Irfanmazuki9@gmail.com>
2021-12-18 05:57:39 +0800

    Finsished working on the category list, add new category, edit category and activate/deactivate category

--- GDEXMSS/Controllers/CategoryController.cs
+++ GDEXMSS/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GDEXMSS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,76 @@ namespace GDEXMSS.Controllers
 {
     public class CategoryController : Controller
     {
+        mssdbModel dbModel = new mssdbModel();
         // GET: Category
         public ActionResult Index()
         {
             return View();
         }
+        [AdminSessionCheck]
+        [HttpGet]
+        public ActionResult List()
+        {
+            var categoryList = (from productCategory in dbModel.productCategories select productCategory).ToList();
+            return View(categoryList);
+        }
+        [AdminSessionCheck]
+        [HttpGet]
+        public ActionResult Edit(int categoryID, string actions)
+        {
+            productCategory categoryModel = new productCategory();
+            if (actions == "edit")
+            {
+                var categoryRecord = dbModel.productCategories.Where(x => x.categoryID == categoryID).FirstOrDefault();
+                categoryModel = categoryRecord;
+                return View(categoryModel);
+            }
+            if (actions == "deactivate")
+            {
+                var categoryRecord = dbModel.productCategories.Where(x => x.categoryID == categoryID).FirstOrDefault();
+                categoryRecord.isExist = false;
+                dbModel.SaveChanges();
+                return RedirectToAction("List");
+            }
+            if (actions == "activate")
+            {
+                var categoryRecord = dbModel.productCategories.Where(x => x.categoryID == categoryID).FirstOrDefault();
+                categoryRecord.isExist = true;
+                dbModel.SaveChanges();
+                return RedirectToAction("List");
+            }
+            return View(categoryModel);
+        }
+
+        [AdminSessionCheck]
+        [HttpPost]
+        public ActionResult Edit(productCategory categoryModel)
+        {
+            var objCategory = dbModel.productCategories.Where(x => x.categoryID == categoryModel.categoryID).FirstOrDefault();
+            dbModel.Entry(objCategory).CurrentValues.SetValues(categoryModel);
+            dbModel.SaveChanges();
+            return RedirectToAction("List");
+        }
+        [AdminSessionCheck]
+        [HttpGet]
+        public ActionResult Add()
+        {
+            productCategory categoryModel = new productCategory();
+            return View(categoryModel);
+        }
+        [AdminSessionCheck]
+        [HttpPost]
+        public ActionResult Add(productCategory categoryModel)
+        {
+            categoryModel.isExist = true;
+            using (mssdbModel dbModel = new mssdbModel())
+            {
+                dbModel.productCategories.Add(categoryModel);
+                dbModel.SaveChanges();
+            }
+               
+            return RedirectToAction("List");
+        }
+
     }
 }
